39e24bdbe51720300e379c3fd6242bb43afc2763
dimatik <D4xGkKpVmr79uhn8mh43>
2024-06-21 14:39:02 +0300

    fix1

--- src/main/java/com/example/bet/redis/load/test/BetRedisLoadTest/repository/DataEntityRepository.java
+++ src/main/java/com/example/bet/redis/load/test/BetRedisLoadTest/repository/DataEntityRepository.java
@@ -2,11 +2,6 @@ package com.example.bet.redis.load.test.BetRedisLoadTest.repository;
 
 import com.example.bet.redis.load.test.BetRedisLoadTest.entity.postgres.DataEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 public interface DataEntityRepository extends JpaRepository<DataEntity, Long> {
-    @Modifying
-    @Query(value = "INSERT INTO data_entityes VALUES (:id, CAST(:json AS json))", nativeQuery = true)
-    void saveJson(long id, String json);
 }
