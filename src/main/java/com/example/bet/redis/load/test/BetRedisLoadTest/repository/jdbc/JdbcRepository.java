7c88611028ddd8b8ab5fe5be95aa2b5416ee6baf
dimatik <D4xGkKpVmr79uhn8mh43>
2024-06-25 09:43:09 +0300

    connection pool for jdbc impl

--- src/main/java/com/example/bet/redis/load/test/BetRedisLoadTest/repository/jdbc/JdbcRepository.java
+++ src/main/java/com/example/bet/redis/load/test/BetRedisLoadTest/repository/jdbc/JdbcRepository.java
@@ -1,5 +1,7 @@
 package com.example.bet.redis.load.test.BetRedisLoadTest.repository.jdbc;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
@@ -14,22 +16,35 @@ public class JdbcRepository {
     private  String username;
     @Value("${spring.datasource.password}")
     private  String password;
-    private Connection connection;
+    //private Connection connection;
+
+    private  HikariDataSource hikariDataSource;
 
     @PostConstruct
     public void setUp(){
-        try {
+        HikariConfig config = new HikariConfig();
+        hikariDataSource = new HikariDataSource();
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+        config.setMaximumPoolSize(30);
+        config.setDriverClassName("org.postgresql.Driver");
+        //config.addDataSourceProperty("cachePrepStmts", "true");
+        //config.addDataSourceProperty("prepStmtCacheSize", "250");
+        //config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        hikariDataSource = new HikariDataSource(config);
+
+        /*try {
             connection = DriverManager.getConnection(url, username, password);
         } catch (SQLException e) {
             throw new RuntimeException(e);
-        }
+        }*/
     }
 
     public String findById(long id){
         String result = null;
 
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT d.data FROM data_entityes d WHERE d.id = ?");
+        try (PreparedStatement preparedStatement = hikariDataSource.getConnection().prepareStatement("SELECT d.data FROM data_entityes d WHERE d.id = ?")){
             preparedStatement.setLong(1, id);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()) {
